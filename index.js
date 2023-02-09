cf4abb8e5ed9cd5fb9c37485d1dea0962bf72924
Nottommy11 <thomas.marxsen11@gmail.com>
2023-02-08 23:27:38 -0600

    Login data added

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
-const importData = require("./questionData/L1M1.json");
+const questionDataL1M1 = require("./questionData/L1M1.json");
+const loginData = require("./loginData/logins.json");
 let port = process.env.PORT || 3005;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/apiData", (req, res) => {
-  res.send(importData);
+app.get("/questionData/L1/M1", (req, res) => {
+  res.send(questionDataL1M1);
+});
+
+app.get("/loginData", (req, res) => {
+  res.send(loginData);
 });
 
 app.listen(port, () => {
