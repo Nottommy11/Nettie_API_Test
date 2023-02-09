dfdfe41d6f871e784c3aa1216cba873cbdbbc83c
Nottommy11 <thomas.marxsen11@gmail.com>
2023-02-09 05:25:41 -0600

    Cors Fix?

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const questionDataL1M1 = require("./questionData/L1M1.json");
 const loginData = require("./loginData/logins.json");
 let port = process.env.PORT || 3005;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/questionData/L1/M1", (req, res) => {
-  res.send(questionDataL1M1);
+  res.status(200).send(questionDataL1M1);
 });
 
 app.get("/loginData", (req, res) => {
-  res.send(loginData);
+  res.status(200).send(loginData);
 });
 
 app.listen(port, () => {
